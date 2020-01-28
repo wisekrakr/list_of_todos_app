cf4d1f4e60f5474e7fcc9e172c3dd62cf6f05ca3
David <wisekrakr@gmail.com>
2020-01-28 06:04:19 +0100

    Updates

---
+++ precache-manifest.9ae9a49e65263ba8a7ed5f61533a2a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d908646f40b2ee77a3da9ee459ed97",
+    "url": "/list_of_todos_app/index.html"
+  },
+  {
+    "revision": "574acee24e6b797d2354",
+    "url": "/list_of_todos_app/static/css/main.f206976d.chunk.css"
+  },
+  {
+    "revision": "52766f5dad3f0ef1dbdb",
+    "url": "/list_of_todos_app/static/js/2.7156f153.chunk.js"
+  },
+  {
+    "revision": "9c8faab933b8fe02833aea8bf1f524cc",
+    "url": "/list_of_todos_app/static/js/2.7156f153.chunk.js.LICENSE"
+  },
+  {
+    "revision": "574acee24e6b797d2354",
+    "url": "/list_of_todos_app/static/js/main.08179dc1.chunk.js"
+  },
+  {
+    "revision": "70d619af88c773553f4e",
+    "url": "/list_of_todos_app/static/js/runtime-main.397501f9.js"
+  }
+]);
